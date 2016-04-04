203358098650a944e458185d782fa18bfa2f0d8c
Anishece <chakrabortyanish2@gmail.com>
2016-04-04 17:15:01 +0530

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,31 @@
+class PostsController < ApplicationController
+  def index
+    @content1 = 'This is a sample page'
+    @content2 = 'This is another sample post'
+  end
+  
+  def new
+    
+  end
+  
+  def create
+    
+  end
+  
+  def edit
+    
+  end
+  
+  def update
+    
+  end
+  
+  def show
+    
+  end
+  
+  def destroy
+    
+  end  
+  
+end
