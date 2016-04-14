c732ad99a4e9cec727643424e0da5c6483643e07
Anishece <chakrabortyanish2@gmail.com>
2016-04-14 12:22:10 +0530

    modified category of admin

--- app/admin/category.rb
+++ app/admin/category.rb
@@ -1,17 +1,27 @@
 ActiveAdmin.register Category do
 
-
+  form :title => { :new => "New Category", :edit => "Renew  Category"} do |f|
+    f.inputs do
+      f.input :name
+     # f.input :description
+    end
+    f.actions do
+      f.action :submit
+      f.action :cancel, :label => 'Cancel', :input_html => {:class => 'cancel_subscription'}
+    end
+  end
+#end
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
-  # permit_params :list, :of, :attributes, :on, :model
+  permit_params :all
   #
   # or
   #
   # permit_params do
   #   permitted = [:permitted, :attributes]
-  #   permitted << :other if resource.something?
-  #   permitted
+  #  permitted << :other if resource.something?
+  #  permitted
   # end
 
 
